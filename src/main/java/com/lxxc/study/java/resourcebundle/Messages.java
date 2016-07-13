69907b276c4ef8fe4dd2a78d7bf67665948c5949
lxc11 <Administrator@www.g-jia.net>
2016-07-13 14:04:59 +0800

    学习使用ResourceBundle

---
+++ src/main/java/com/lxxc/study/java/resourcebundle/Messages.java
@@ -0,0 +1,44 @@
+package com.lxxc.study.java.resourcebundle;
+
+import java.text.MessageFormat;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+public class Messages {
+	private static final String BUNDLE_NAME = "com.lxxc.study.java.resourcebundle.messages";
+	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
+	private Messages(){
+	}
+	
+	public static String getString(String key){
+		try{
+			return RESOURCE_BUNDLE.getString(key);
+		}catch(MissingResourceException e){
+			return '!' + key + '!';
+		}
+	}
+	
+    public static String getString(String key, String parm1) {
+        try {
+            return MessageFormat.format(RESOURCE_BUNDLE.getString(key),
+                    new Object[] { parm1 });
+        } catch (MissingResourceException e) {
+            return '!' + key + '!';
+        }
+    }
+
+    public static String getString(String key, String parm1, String parm2) {
+        try {
+            return MessageFormat.format(RESOURCE_BUNDLE.getString(key),
+                    new Object[] { parm1, parm2 });
+        } catch (MissingResourceException e) {
+            return '!' + key + '!';
+        }
+    }
+    
+    public static void main(String[] args) {
+		System.out.println(getString("name"));
+		System.out.println(getString("age", "28"));
+		System.out.println(getString("habit", "study", "look"));
+	}
+}
